d529a89662c3b0889ade1245a4344749e22cb69c
Christopher Scott <cscottdev@protonmail.com>
2024-08-27 14:28:46 -0500

    updated handling of esplora urls per network

--- api/mempool.ts
+++ api/mempool.ts
@@ -1,15 +1,21 @@
 import axios from 'axios';
 import { NetworkType, RecommendedFeeResponse } from '../types';
 
+const networkHostMap = {
+  Mainnet: 'https://mempool.space/',
+  Testnet: 'http://localhost:3002/',
+  Signet: 'https://mempool.space/',
+};
+
 const networkRouteMap = {
-  Mainnet: '',
-  Testnet: 'testnet/',
-  Signet: 'signet/',
+  Mainnet: 'api/',
+  Testnet: '',
+  Signet: 'signet/api/',
 };
 
 const getRecommendedFees = async (network: NetworkType): Promise<RecommendedFeeResponse> => {
   const { data } = await axios.get<RecommendedFeeResponse>(
-    `https://mempool.space/${networkRouteMap[network]}api/v1/fees/recommended`,
+    `${networkHostMap[network]}${networkRouteMap[network]}v1/fees/recommended`,
   );
   return data;
 };
